3fa45115d12861772ce844f5171f7a7ca00c10ec
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-16 11:46:13 +0900

    changed input and output

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ let io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname))
 
-server.listen(process.env.PORT);
+server.listen(5000);
 
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/send', (req, res)=> {
-    res.sendFile(__dirname + '/vies/send.html');
+    res.sendFile(__dirname + '/views/send.html');
 });
 
 io.sockets.on('connection', (socket)=>{
