b8793e1a62d06f3ce8ed17d9e48851a168b4a892
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-16 11:39:37 +0900

    added sender

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/send', (req, res)=> {
+    res.sendFile(__dirname + '/vies/send.html');
+});
+
 io.sockets.on('connection', (socket)=>{
     socket.on('message', (data)=>{
         io.sockets.emit("message",
