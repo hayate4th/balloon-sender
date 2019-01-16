84f7deecb21f1c7eb64b14683d4794c7f8c7d3bb
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-16 13:36:34 +0900

    unable to input empty strings

--- js/index.js
+++ js/index.js
@@ -19,7 +19,9 @@ var addMessage = function(msg){
 }
 
 var makeMessage = function(msg){
-    rnd_top = 25 - Math.floor( Math.random () * 25) ;
-    rnd_left = 80 - Math.floor( Math.random () * 60) ;
-    return "<div style='text-align: right'><div class='my-msg' style='top: " + rnd_top.toString() + "%'><span class='message'>" + msg + "</span></div><div class='my-msg-tree' style='top: " + rnd_top.toString() + "%; left: " + rnd_left.toString() + "%'></div></div></br>"
+    if (msg.length > 0) {
+        rnd_top = 25 - Math.floor( Math.random () * 25) ;
+        rnd_left = 80 - Math.floor( Math.random () * 60) ;
+        return "<div style='text-align: right'><div class='my-msg' style='top: " + rnd_top.toString() + "%'><span class='message'>" + msg + "</span></div><div class='my-msg-tree' style='top: " + rnd_top.toString() + "%; left: " + rnd_left.toString() + "%'></div></div></br>"
+    }
 }
