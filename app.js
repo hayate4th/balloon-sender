58340d4c13ee8d274cbfcbada35321ce90037388
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-16 13:26:48 +0900

    added basic authorization

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ let http = require('http')
 let app = express()
 let server = http.createServer(app)
 let io = require('socket.io').listen(server);
+let auth = require('basic-auth');
+let admins = {
+    'admin': { password: 'Kb893veM' }
+};
 
 app.use(express.static(__dirname))
 
-server.listen(process.env.PORT);
-// server.listen(5000);
+// server.listen(process.env.PORT);
+server.listen(5000);
 
 app.get('/', (req, res)=> {
+    var user = auth(req);
+    if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+      res.set('WWW-Authenticate', 'Basic realm="example"');
+      return res.status(401).send();
+    }
     res.sendFile(__dirname + '/views/index.html');
 });
 
