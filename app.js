af69389926af420c0f9c7087f1549b2552b969c0
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-18 13:33:40 +0900

    not allowing access

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ app.use(express.static(__dirname))
 server.listen(process.env.PORT);
 // server.listen(5000);
 
-app.get('/', (req, res)=> {
-    var user = auth(req);
-    if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
-      res.set('WWW-Authenticate', 'Basic realm="example"');
-      return res.status(401).send();
-    }
-    res.sendFile(__dirname + '/views/index.html');
-});
+// app.get('/', (req, res)=> {
+//     var user = auth(req);
+//     if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+//       res.set('WWW-Authenticate', 'Basic realm="example"');
+//       return res.status(401).send();
+//     }
+//     res.sendFile(__dirname + '/views/index.html');
+// });
 
 // app.get('/send', (req, res)=> {
 //     res.sendFile(__dirname + '/views/send.html');
