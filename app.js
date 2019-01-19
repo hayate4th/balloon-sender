24f64ed725ceae77c2bbfde7605c6ec0e5fb2980
hayate4th <rnakagawa@kde.cs.tsukuba.ac.jp>
2019-01-19 11:28:32 +0900

    release 2

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ filter = new Filter()
 
 app.use(express.static(__dirname))
 
-// server.listen(process.env.PORT);
-server.listen(5000);
+server.listen(process.env.PORT);
+/server.listen(5000);
 
 app.get('/', (req, res)=> {
     var user = auth(req);
@@ -27,9 +27,9 @@ app.get('/', (req, res)=> {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get('/send', (req, res)=> {
-    res.sendFile(__dirname + '/views/send.html');
-});
+// app.get('/send', (req, res)=> {
+//     res.sendFile(__dirname + '/views/send.html');
+// });
 
 io.sockets.on('connection', (socket)=>{
     socket.on('message', (data)=>{
