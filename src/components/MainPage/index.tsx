1ec537887e698a210bc547fddc680f331beeeefb
hayate4th <diabolo.arte3000@gmail.com>
2020-06-10 22:56:20 +0900

    Remove SendPage component and moved the sending UI to MainPage

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -13,19 +13,43 @@ type SocketData = {
 
 const MainPage: React.FC = () => {
   const [response, setResponse] = useState("");
+  const [inputText, setInputText] = useState("");
+
+  const socket = socketIOClient(ENDPOINT);
 
   useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
     socket.on("message", (data: SocketData) => {
       setResponse(data.message);
     });
-  }, []);
+  }, [socket]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSendClick = () => {
+    socket.emit("message", {
+      message: inputText,
+    });
+    setInputText("");
+  };
 
   return (
     <FlexWrapper>
       {response !== "" && <div>{response}</div>}
       <TreeImg src={tree} />
       <BalloonCounter count={123} />
+      <SenderWrapper>
+        <SenderTitle>Write Something Here</SenderTitle>
+        <MessageInput
+          type="text"
+          name="message-input"
+          placeholder="Input Here"
+          onChange={handleInputChange}
+          value={inputText}
+        />
+        <SendButton onClick={handleSendClick}>Send</SendButton>
+      </SenderWrapper>
     </FlexWrapper>
   );
 };
@@ -36,12 +60,62 @@ const FlexWrapper = styled.div`
   justify-content: center;
   background-color: #ccf1ff;
   width: 100vw;
-  height: calc(100vh - 50px);
+  height: 100vh;
 `;
 
 const TreeImg = styled.img`
   height: 100%;
   width: auto;
+`;
+
+const SenderWrapper = styled.div`
+  position: absolute;
+  right: 5%;
+  bottom: 5%;
+  border-radius: 5px;
+  background-color: #ee7f00;
+  padding: 10px;
+`;
+
+const SenderTitle = styled.h2`
+  color: white;
+  font-size: 25px;
+  margin: 0;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+const MessageInput = styled.input`
+  border: 0px;
+  border-bottom: 2px solid white;
+  background: none;
+  color: white;
+  font-size: 25px;
+  font-family: "Itim", cursive;
+  outline: none;
+  max-width: 500px;
+  margin-right: 10px;
+
+  &::placeholder {
+    color: white;
+    opacity: 0.7;
+  }
+`;
+
+const SendButton = styled.button`
+  outline: none;
+  font-size: 25px;
+  font-family: "Itim", cursive;
+  padding: 5px;
+  color: white;
+  border: 2px solid white;
+  border-radius: 3px;
+  background-color: transparent;
+
+  &:active {
+    color: #ee7f00;
+    background-color: white;
+  }
 `;
 
 export default MainPage;
