03733005ea952a18724ca36303d70b41f4c43f79
hayate4th <diabolo.arte3000@gmail.com>
2020-06-11 23:35:15 +0900

    Change send button to be disabled if input is empty

--- src/components/BalloonSender/index.tsx
+++ src/components/BalloonSender/index.tsx
@@ -29,7 +29,9 @@ const BalloonSender: React.FC<Props> = ({ socket }) => {
         onChange={handleInputChange}
         value={inputText}
       />
-      <SendButton onClick={handleSendClick}>Send</SendButton>
+      <SendButton onClick={handleSendClick} disabled={inputText === ""}>
+        Send
+      </SendButton>
     </SenderWrapper>
   );
 };
