2b9268996505074e0443d2a6af92202d2985e762
hayate4th <diabolo.arte3000@gmail.com>
2020-06-13 13:59:05 +0900

    Fix css properties to line up alphabetically

--- src/components/BalloonCounter/index.tsx
+++ src/components/BalloonCounter/index.tsx
@@ -22,17 +22,17 @@ const BalloonCounter: React.FC<Props> = ({ count }) => {
 };
 
 const Wrapper = styled.div`
-  position: absolute;
-  left: 10%;
-  bottom: 10%;
-  background-color: white;
+  background-color: #fff;
   border: 1px solid #eeeeee;
   border-radius: 5px;
-  padding: 10px 10px 20px;
+  bottom: 10%;
+  left: 10%;
+  padding: 10px 10px 20px 10px;
+  position: absolute;
 `;
 
 const CounterTitle = styled.h2`
-  font-size: 15px;
+  font: 15px "Itim", cursive;
   margin: 0 0 10px 0;
   text-align: center;
 `;
@@ -50,10 +50,9 @@ const Counter = styled.div`
 `;
 
 const Digit = styled.div`
-  font-size: 25px;
-  font-weight: bold;
-  color: white;
   background-color: #ee7f00;
+  color: #fff;
+  font: 25px "Itim", cursive;
   padding: 5px 10px;
 `;
 
